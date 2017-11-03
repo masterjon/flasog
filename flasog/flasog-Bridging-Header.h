4a6e5aff6ef0bd50a3a7631465e0c7b67f84bb29
Jonathan Horta <jonabc7@gmail.com>
2017-11-03 03:21:13 -0500

    final version

---
+++ flasog/flasog-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  flasog-Bridging-Header.h
+//  flasog
+//
+//  Created by Jonathan Horta on 10/28/17.
+//  Copyright © 2017 iddeas. All rights reserved.
+//
+
+#ifndef flasog_Bridging_Header_h
+#define flasog_Bridging_Header_h
+
+
+#endif /* flasog_Bridging_Header_h */
